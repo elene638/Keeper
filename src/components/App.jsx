24eb8311c8ce5be33a1528a1b810e54873664615
elene638 <elenejavaxishvili@yahoo.com>
2022-05-17 16:01:07 -0700

    added function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import Note from "./Note"
+import Note from "./Note";
 import Footer from "./Footer";
 import Header from "./Header";
+import notes from "../notes";
 
-function App(){
+
+
+function App() {
     return (
+      <div>
+        <Header />
         <div>
-            <Header />
-            <Note />
-            <Footer />
+          {notes.map(individualNote => 
+              <Note
+                key={individualNote.key}
+                titles={individualNote.title}
+                contents={individualNote.content}
+              />
+          )}
         </div>
+        <Footer />
+      </div>
     );
-}
+  }
 
 export default App;
+
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note () {
+
+function Note (props) {
     return (
         <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+            <h1>{props.titles}</h1>
+            <p>{props.contents}</p>
         </div>
     );
 }
